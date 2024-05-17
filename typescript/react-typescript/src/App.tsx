6a676e55ebf103ca604125431342e9a9b84d0990
adakasnr <adakasnr@gmail.com>
2024-05-17 22:20:30 +0530

    hooks

--- typescript/react-typescript/src/App.tsx
+++ typescript/react-typescript/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Increment from './Hooks/Increment';
+import Increment from './Hooks/useState/Increment';
+import Index from './Hooks/useState/UseState-adv';
 
 function App() {
   return (
     <main className='container'>
       <button className='button'>click me</button>
         <Increment />
+        <Index />
     </main>
   )
 }

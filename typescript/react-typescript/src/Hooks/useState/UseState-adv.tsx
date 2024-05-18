65019f49b9d52568e30e0b6e695dc3315120f830
adakasnr <adakasnr@gmail.com>
2024-05-18 16:22:54 +0530

    Conditional Rendering

--- typescript/react-typescript/src/Hooks/useState/UseState-adv.tsx
+++ typescript/react-typescript/src/Hooks/useState/UseState-adv.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 const Index: React.FC = () => {
-    const intialObj: { firstName: string; lastName: string; age:number } = {
+    const intialObj: { firstName: string; lastName: string; age: number } = {
         firstName: "emma",
         lastName: "watson",
-        age:27 
+        age: 27
     }
 
     const [data, setData] = useState(intialObj)
@@ -23,9 +23,14 @@ const Index: React.FC = () => {
     const changeAge = (): void => {
         setData({
             ...data,
-            age:28
+            age: 28
         })
     }
+
+    const [showdata, setShowData] = useState(false)
+    const handleChange = () => {
+        setShowData(!showdata);
+    };
 
     return (
         <div>
@@ -35,6 +40,13 @@ const Index: React.FC = () => {
             <button onClick={changeLastName}>changeLastName</button>
             <h2>age {data.age}</h2>
             <button onClick={changeAge}>age </button>
+            {/*Conditional Rendering*/}
+            <button onClick={handleChange}>{showdata ? "hide" : "show"}</button>
+            {showdata && (
+                <div className="content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat sed adipisci rem illo cum cumque debitis autem architecto eligendi, et nostrum voluptas quaerat. Ut praesentium doloribus at illum libero.
+                </div>
+            )}
         </div>)
 }
 
